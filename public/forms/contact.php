aa829e926f7eab82b84d92c9e479dcaa868afb72
Vinay Patidar <vpatidar94@gmail.com>
2022-05-02 12:50:10 +0530

    email

--- public/forms/contact.php
+++ public/forms/contact.php
@@ -9,31 +9,23 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'ankit@kesariautomation.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  if(isset($_POST['submit'])){
+    $to = "ankit@kesariautomation.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
 
-  Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  $contact->smtp = array(
-    'host' => 'smtpout.secureserver.net',
-    'username' => 'ak93patidar@gmail.com',
-    'password' => 'ankit@210patidar',
-    'port' => '465'
-  );
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
